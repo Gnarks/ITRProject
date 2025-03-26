20d990daa838c767a04d3f1afb0b3c1d73719528
Gnarks <231026@umons.ac.be>
2025-03-26 09:46:28 +0100

    working commit

--- main.c
+++ main.c
@@ -5,17 +5,17 @@
 
 int main(int argc, char *argv[])
 { 
-  struct assembly_line line;
-  init_assembly_line(line); 
+  assembly_line_t line;
+  init_assembly_line(&line); 
 
-  setup_arm(line, PART_FRAME, LEFT, 1);
-  setup_arm(line, PART_ENGINE, LEFT, 2);
-  setup_arm(line, PART_WHEELS, RIGHT, 2);
-  setup_arm(line, PART_BODY, LEFT, 3);
-  setup_arm(line, PART_DOORS, LEFT, 4);
-  setup_arm(line, PART_LIGHTS, RIGHT,4);
-  setup_arm(line, PART_WINDOWS, LEFT, 5);
+  /*setup_arm(line, PART_FRAME, LEFT, 1);*/
+  /*setup_arm(line, PART_ENGINE, LEFT, 2);*/
+  /*setup_arm(line, PART_WHEELS, RIGHT, 2);*/
+  /*setup_arm(line, PART_BODY, LEFT, 3);*/
+  /*setup_arm(line, PART_DOORS, LEFT, 4);*/
+  /*setup_arm(line, PART_LIGHTS, RIGHT,4);*/
+  /*setup_arm(line, PART_WINDOWS, LEFT, 5);*/
 
-  free_assembly_line(line);
+  free_assembly_line(&line);
   return EXIT_SUCCESS;
 }
