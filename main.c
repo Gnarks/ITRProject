9ba3eade9a04fb501f3d98db4539064b43691d6a
Gnarks <231026@umons.ac.be>
2025-03-26 09:34:22 +0100

    added mekefile

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-#include "files/assembly.h"
+#include "dependencies/assembly.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 { 
-  struct assembly_line *line;
-  init_assembly_line(line); 
-  
-
-  free_assembly_line(line);
-  return EXIT_SUCCESS;
 }
