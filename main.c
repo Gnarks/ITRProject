d305d817e07d1bb721f2d4004bdf2cd6f5daba7f
Léo Moulin <46688478+LeoMoulin@users.noreply.github.com>
2025-03-27 15:37:11 +0100

    Update de main.c et ajout des timers (pas entièrement fonctionnels)

--- main.c
+++ main.c
@@ -5,23 +5,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 typedef struct {
-  side_t side;
-  unsigned int position; 
-}armId ;
+  assembly_line_t line;
+  unsigned int position;
+}timerargs ;
 
 
-static void timer_handler(union sigval sigev_value) {
-  armId* id = sigev_value.sival_ptr;
+static void timer_handler(union sigval sv) {
+  timerargs* id = sv.sival_ptr;
 
-  // here call the trigger_arm 
+  // here call the trigger_arm
+  error_t leftarm = trigger_arm(id->line,LEFT, id->position);
+  error_t rightarm = trigger_arm(id->line,RIGHT, id->position);
   // todo en fct de la sortie error_t faire des trucs
-
+  id->position++;
+  printf("lefterror : %d \n", leftarm);
+  printf("righterror: %d \n", rightarm);
+  printf("position is %d \n", id->position);
 }
 
 int main(int argc, char *argv[])
-{ 
+{
   assembly_line_t line;
   init_assembly_line(&line);
 
@@ -33,28 +37,31 @@ int main(int argc, char *argv[])
   setup_arm(line, PART_LIGHTS, RIGHT,4);
   setup_arm(line, PART_WINDOWS, LEFT, 5);
 
-  run_assembly(line);
-
   //Setup du timer
   timer_t timefunc;
   struct sigevent funev;
   struct itimerspec funspec;
 
+  //Arguments initiaux du timer_handle
+  timerargs args = {line, 0};
+
+  //Config du timer
   funev.sigev_notify = SIGEV_THREAD;
-
-  //Fonction a trigger 
   funev.sigev_notify_function = timer_handler;
-
-  //Todo gérer plusieurs attributs de la fonction trigger_arm
   funev.sigev_notify_attributes = NULL;
-
-  funspec.it_value.tv_nsec=0;
-  //Intervalle
-  funspec.it_interval.tv_nsec=BELT_PERIOD*1000000;
+  funev.sigev_value.sival_ptr = &args;
+  
+  funspec.it_value.tv_sec=0;
+  funspec.it_value.tv_nsec=1;
+  funspec.it_interval.tv_nsec=(BELT_PERIOD%1000)*1000000;
+  funspec.it_interval.tv_sec=BELT_PERIOD/1000;
 
   timer_create(CLOCK_REALTIME, &funev, &timefunc);
+
+  //Lancement du timer
   timer_settime(timefunc, 0, &funspec, NULL);
 
+  run_assembly(line);
   free_assembly_line(&line);
   return EXIT_SUCCESS;
 }
